96ac973f6313931b50ac5c268ce641463b27fa3e
HeitorDalla <heitorvillavilla@gmail.com>
2025-06-11 09:15:07 -0300

    Subindo alteracoes dentro da main principal

--- main.py
+++ main.py
@@ -2,44 +2,52 @@ import streamlit as st
 import pandas as pd
 from modules.data import dados_tratados
 from modules.database import initialize_database
-from modules.pages import dashboard, graficos
+from modules.pages import dashboard, cargas, aeroportos, regioes, rotas
 
 # Inicializar o banco de dados
 conn, cursor = initialize_database()
+
+dados = dados_tratados()
 
 # Streamlit
 
 # Configurações Gerais da Página
 st.set_page_config(page_title="Dashboard ANAC", layout="wide")
 
-# Sidebar
-st.sidebar.title("🔧 Filtros")
-st.sidebar.markdown("## Personalize a sua Análise")
+# Sidebar - Filtros Globais
+# st.sidebar.image("img/logo_anac.png", width=180)
+st.sidebar.markdown("### Agência Nacional de Aviação Civil")
 
-st.sidebar.markdown("#### Período")
+# Filtros aplicáveis a todas as páginas
+mes_unicos = st.sidebar.selectbox("Mês", sorted(dados["MÊS"].unique()))
+empresa_unicas = st.sidebar.multiselect("Empresa Aérea", dados["EMPRESA (NOME)"].unique())
+uf_origem_unicos = st.sidebar.multiselect("UF Origem", dados["AEROPORTO DE ORIGEM (UF)"].unique())
 
-with st.sidebar:
-    ano_selecionado = st.selectbox(
-        'Selecione o ano:',
-        options = [2020, 2021, 2022, 2023, 2024, 2025],
-        index = 0
-    )
+# Aplicar os filtros aos dados
+filtro = dados.copy() # cópia do dados
+if mes_unicos:
+    filtro = filtro[filtro["MÊS"] == mes_unicos]
+if empresa_unicas:
+    filtro = filtro[filtro["EMPRESA (NOME)"].isin(empresa_unicas)]
+if uf_origem_unicos:
+    filtro = filtro[filtro["AEROPORTO DE ORIGEM (UF)"].isin(uf_origem_unicos)]
 
-    if "menu_ativo" not in st.session_state:
-        st.session_state.menu_ativo = "Dashboard"
+# ADICIONAR BOTÃO DE RESETAR TODOS OS FILTROS -----
 
-    if st.sidebar.button("Dashboard", type='tertiary'):
-        st.session_state.menu_ativo = "Dashboard"
-        
-    if st.sidebar.button("Dados Estatisticos", type='tertiary'):
-        st.session_state.menu_ativo = "Dados Estatisticos"
+# Navegação dentro das páginas principais
+abas = st.tabs(["🏠 Visão Geral", "🗺️ Regiões", "✈️ Aeroportos", "📦 Cargas", "🔁 Rotas"])
 
-    menu = st.session_state.menu_ativo
+with abas[0]:
+    dashboard.renderizar(filtro)
 
-# Página Principal
-dados = dados_tratados()
+with abas[1]:
+    regioes.renderizar(filtro)
 
-if menu == "Dashboard":
-    dashboard(dados)
-elif menu == "Dados Estatisticos":
-    graficos(dados)
+with abas[2]:
+    aeroportos.renderizar(filtro)
+
+with abas[3]:
+    cargas.renderizar(filtro)
+
+with abas[4]:
+    rotas.renderizar(filtro)
